3d14d641032d3b909b13c1bbf6eaff7cd21b13c7
aaadddiii <adithya.kurien@gmail.com>
2020-06-23 18:41:17 +0530

    Updates

---
+++ precache-manifest.fb1bf4f6dca3b607aa8ddadbd493ca02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afb4301456f0e524dd43a3d034cc69e",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "bcef60b2377a5fa1a9e1",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a9a06c49bd0787545315",
+    "url": "/facerecognition/static/css/main.b417ba86.chunk.css"
+  },
+  {
+    "revision": "bcef60b2377a5fa1a9e1",
+    "url": "/facerecognition/static/js/2.b15461ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.b15461ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a06c49bd0787545315",
+    "url": "/facerecognition/static/js/main.1932ae83.chunk.js"
+  },
+  {
+    "revision": "705f038354876f9bd8eb",
+    "url": "/facerecognition/static/js/runtime-main.5cfe2c86.js"
+  }
+]);
